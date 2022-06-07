d51463ca0bad444aa0ef212557bd937bfe7f0769
gferrari18 <gferrari18@my.whitworth.edu>
2022-06-06 20:40:20 -0700

    removed unnecessary imports

--- DurationUpdater.py
+++ DurationUpdater.py
@@ -3,11 +3,8 @@ import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from pandas import DataFrame
 import pandas as pd
 
 
--- LMSClicker.py
+++ LMSClicker.py
@@ -1,7 +1,6 @@
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
 usrid = input("Enter user ID: ")
